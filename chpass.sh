4cc6513a12d38e1f27b57af128bdc0390182a42c
Guido Kroon <guido@kroon.email>
2019-04-04 13:16:23 +0200

    chpass.sh: small code optimisations

--- chpass.sh
+++ chpass.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 print_help() {
   echo "chpass, version 0.1"
@@ -29,7 +29,7 @@ print_help() {
 }
 
 check_sudo() {
-  if [ `whoami` != root ]; then
+  if [ "$(whoami)" != root ]; then
     echo "Please run this script using sudo. Exiting..."
     exit 1
   fi
@@ -44,8 +44,8 @@ get_args() {
       -u|--username) USER="${2}" ;;
       -l|--login) LOGIN="1" ;;
       -r|--root) ROOT="1" ;;
-      -s|--ssh-key) SSH_KEY=$(readlink -m ${2}) ;;
-      -L|--luks-gpg-key) LUKS_GPG_KEY=$(readlink -m ${2}) ;;
+      -s|--ssh-key) SSH_KEY=$(readlink -m "${2}") ;;
+      -L|--luks-gpg-key) LUKS_GPG_KEY=$(readlink -m "${2}") ;;
       -g|--gpg-key-id) GPG_KEY_ID="{2}" ;;
     esac
     shift
@@ -54,50 +54,56 @@ get_args() {
 
 get_vars() {
   # Terminal colours
-  GREEN='\033[0;32m'
+  #GREEN='\033[0;32m'
   BLUE='\033[0;34m'
-  MAGENTA='\033[0;35m'
+  #MAGENTA='\033[0;35m'
   LGREEN='\033[1;32m'
   LRED='\033[1;31m'
-  YELLOW='\033[1;33m'
+  #YELLOW='\033[1;33m'
   NC='\033[0m'
 
-  # Notification prefix
-  NOTIFY_PREFIX=">>> Setting new passwd for:"
+  # Print "[ !! ]" or "[ OK ]" in colour
+  FAILED="${BLUE}[${NC} ${LRED}!!${NC} ${BLUE}]${NC}"
+  OK="${BLUE}[${NC} ${LGREEN}ok${NC} ${BLUE}]${NC}"
 }
 
 test_args() {
-  if [ ! -n "${OLD_PASSWD}" ]; then
+  if [ -z "${OLD_PASSWD}" ]; then
     echo "-o|--old-passphrase no set. Exiting..".
     exit 1
   fi
 
-  if [ ! -n "${NEW_PASSWD}" ]; then
+  if [ -z "${NEW_PASSWD}" ]; then
     echo "-n|--new-passphrase no set. Exiting..".
     exit 1
   fi
 
-  if [ ! -n "${USER}" ]; then
+  if [ -z "${USER}" ]; then
     USER="${SUDO_USER:-${USER}}"
   fi
 }
 
 user_passwd() {
   if ! echo "${USER}:${NEW_PASSWD}" | chpasswd; then
-    echo -e "[${LRED}FAILED${NC}]: did you run as root?"
+    printf "%s\n\nCould not change user passwd. Exiting...\n" "${FAILED}"
+    exit 1
   fi
 }
 
 root_passwd() {
   if ! echo "root:${NEW_PASSWD}" | chpasswd; then
-    echo -e "[${LRED}FAILED${NC}]: did you run as root?"
+    printf "%s\n\nCould not change root passwd. Exiting...\n" "${FAILED}"
+    exit 1
   fi
 }
 
 ssh_key() {
+  # First try to check if NEW_PASSWD is already set.
   if ! ssh-keygen -q -p -P "${NEW_PASSWD}" -N "${NEW_PASSWD}" -f "${SSH_KEY}" >/dev/null 2>&1; then
+    # If not, then we change the passphrase using OLD_PASSWD
     if ! ssh-keygen -q -p -P "${OLD_PASSWD}" -N "${NEW_PASSWD}" -f "${SSH_KEY}" >/dev/null 2>&1; then
-      echo -e "[${LRED}FAILED${NC}]: is OLD_PASSWD correct?"
+      printf "%s\n\nCould not change SSH passphrase. Is the OLD_PASSWD correct? Exiting...\n" "${FAILED}"
+      exit 1
     fi
   fi
 }
@@ -105,22 +111,21 @@ ssh_key() {
 luks_gpg_key() {
   mount /boot 2>/dev/null
   if ! gpg --quiet --decrypt --batch --passphrase "${NEW_PASSWD}" "${LUKS_GPG_KEY}" >/dev/null 2>&1; then
-    echo $?
     if gpg --quiet --decrypt --batch --passphrase "${OLD_PASSWD}" "/boot/${LUKS_GPG_KEY}" >/dev/null 2>&1; then
     cp "/boot/${LUKS_GPG_KEY}" "/root/old-root.gpg"
       if gpg --quiet --decrypt --batch --passphrase "${OLD_PASSWD}" "/root/old-root.gpg" | gpg --quiet --batch --symmetric --passphrase "${NEW_PASSWD}" -o "/root/new-root.gpg" > /dev/null 2>&1; then
         if gpg --quiet --batch --decrypt --no-symkey-cache --passphrase "${NEW_PASSWD}" "/root/new-root.gpg" > /dev/null 2>&1; then
           mv "/root/new-root.gpg" "/boot/${LUKS_GPG_KEY}" && rm "/root/old-root.gpg" 2>/dev/null
         else
-          echo -e "[${LRED}FAILED${NC}]: NEW_PASSWD is used to create new LUKS_GPG_KEY, but could not be used to decrypt new LUKS_GPG_KEY??? Exiting..."
+          printf "%s/n/nNEW_PASSWD is used to create new LUKS_GPG_KEY, but could not be used to decrypt new LUKS_GPG_KEY??? Exiting..." "${FAILED}"
           exit 1
         fi
       else
-        echo -e "[${LRED}FAILED${NC}]: OLD_PASSWD is correct, but could not create new LUKS_GPG_KEY with NEW_PASSWD? Exiting..."
+        printf "%s/n/nOLD_PASSWD is correct, but could not create new LUKS_GPG_KEY with NEW_PASSWD? Exiting..." "${FAILED}"
         exit 1
       fi
     else
-      echo -e "[${LRED}FAILED${NC}]: is OLD_PASSWD correct? Exiting..."
+      printf "%s/n/nis OLD_PASSWD correct? Exiting..." "${FAILED}"
       exit 1
     fi
   fi
@@ -129,7 +134,7 @@ luks_gpg_key() {
 
 gpg_key() {
   if ! gpg --homedir "/home/${USER}/.gnupg" --batch --passwd "${GPG_KEY_ID}"; then
-    echo -e "[${LRED}FAILED${NC}]: could not change GPG key passphrase. Exiting..."
+    printf "%s/n/ncould not change GPG key passphrase. Exiting..." "${FAILED}"
     exit 1
   fi
 }
@@ -140,39 +145,36 @@ test_args
 check_sudo
 
 if [ -n "${LOGIN}" ]; then
-  echo -n '>>> Changing user passwd .................................. '
+  printf ' %s*%s Changing user passwd ... \t' "${LGREEN}" "${NC}" | expand_width
   if user_passwd ; then
-    echo -e "${BLUE}[${NC} ${LGREEN}ok${NC} ${BLUE}]${NC}"
+    printf "%s\n" "${OK}"
   fi
 fi
 
 if [[ "${ROOT}" -eq "1" ]]; then
-  echo -n '>>> Changing root passwd .................................. '
+    printf ' %s*%s Changing root passwd ... \t' "${LGREEN}" "${NC}" | expand_width
   if root_passwd ; then
-    echo -e "${BLUE}[${NC} ${LGREEN}ok${NC} ${BLUE}]${NC}"
+    printf "%s\n" "${OK}"
   fi
 fi
 
 if [ -n "${SSH_KEY}" ]; then
-  echo -n '>>> Changing SSH key passwd ............................... '
+  printf ' %s*%s Changing SSH key passwd ... \t' "${LGREEN}" "${NC}" | expand_width
   if ssh_key ; then
-    echo -e "${BLUE}[${NC} ${LGREEN}ok${NC} ${BLUE}]${NC}"
+    printf "%s\n" "${OK}"
   fi
 fi
 
 if [ -n "${LUKS_GPG_KEY}" ]; then
-  echo -n '>>> Changing LUKS key passwd .............................. '
+  printf ' %s*%s Changing LUKS key passwd ... \t' "${LGREEN}" "${NC}" | expand_width
   if luks_gpg_key ; then
-    echo -e "${BLUE}[${NC} ${LGREEN}ok${NC} ${BLUE}]${NC}"
+    printf "%s\n" "${OK}"
   fi
 fi
 
 if [ -n "${GPG_KEY_ID}" ]; then
-  echo
-  echo '--- Changing GPG key passwd (manual procedure) --- '
-  echo
+  printf '\n--- Changing GPG key passwd (manual procedure) ---\n\n'
   gpg_key
 fi
 
-echo
-echo "Changing passphrases complete."
+printf "Changing passphrases complete.\n"
